29e6d895a0fc4506c252ba7efd16ae40af5a3b11
xm634634 <xm634634@gmail.com>
2025-11-07 23:53:40 +0800

    Site updated: 2025-11-07 23:53:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/06/hello-world/"];function toRandomPost(){
+var posts=["2025/11/07/How to Get Rich-CN1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
