25fdb5cf511cb6cf41c6169f61906b5bac71a89d
xm634634 <xm634634@gmail.com>
2025-11-13 23:56:48 +0800

    Site updated: 2025-11-13 23:56:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/12/How to Get Rich-1.财富基础认知/"];function toRandomPost(){
+var posts=["2025/11/12/How to Get Rich-CN/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
