8fbaadbdfb509373febaa988681e6d999ffe9ec1
xm634634 <xm634634@gmail.com>
2025-12-04 20:01:42 +0800

    Site updated: 2025-12-04 20:01:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/16/How to Get Rich-2/","2025/11/12/How to Get Rich-1/"];function toRandomPost(){
+var posts=["2025/12/04/How to Get Rich-6/","2025/12/04/How to Get Rich-5/","2025/12/04/How to Get Rich-4/","2025/12/04/How to Get Rich-3/","2025/11/16/How to Get Rich-2/","2025/11/12/How to Get Rich-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
