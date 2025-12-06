3bcd84a8ca2dc05365488f5f1b74c80c51d29def
xm634634 <xm634634@gmail.com>
2025-12-06 23:26:22 +0800

    Site updated: 2025-12-06 23:26:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/04/How to Get Rich-6/","2025/12/04/How to Get Rich-5/","2025/12/04/How to Get Rich-4/","2025/12/04/How to Get Rich-3/","2025/11/16/How to Get Rich-2/","2025/11/12/How to Get Rich-1/"];function toRandomPost(){
+var posts=["2025/12/30/How to Get Rich-5/","2025/12/04/How to Get Rich-6/","2025/11/26/How to Get Rich-4/","2025/11/22/How to Get Rich-3/","2025/11/16/How to Get Rich-2/","2025/11/12/How to Get Rich-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
